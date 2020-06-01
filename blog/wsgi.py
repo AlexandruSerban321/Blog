cfa7cea6308ced2fc95f77eed128a63a64c8890b
alexandru <alexandruserban160@gmail.com>
2020-06-02 01:52:03 +0300

    modified the wsgi file to compatible with pythonanywhere

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 load_dotenv()
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.local_settings')
-
 application = get_wsgi_application()
