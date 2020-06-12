17e5d19b0a82ec445f3cfda57bb93a43b257e522
alexandru <alexandruserban160@gmail.com>
2020-06-12 18:15:05 +0300

    removed test email

--- templates/mail/contact_me.php
+++ templates/mail/contact_me.php
@@ -14,11 +14,11 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-$to = 'serban200208@gmail.com'; 
+$to = ''; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@homeworkblog.com\n";
-$headers .= "Reply-To: $email_address";   
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
