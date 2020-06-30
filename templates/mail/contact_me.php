b4b84cd2f6ab9a9567b2411251d4c3a42a06feab
alexandru <alexandruserban160@gmail.com>
2020-06-30 14:14:07 +0300

    removed phone number from contact me form

--- templates/mail/contact_me.php
+++ templates/mail/contact_me.php
@@ -1,7 +1,6 @@
 <?php
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -11,7 +10,6 @@ if(empty($_POST['name'])      ||
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 $to = ''; 
